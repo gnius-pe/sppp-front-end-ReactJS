0b95c94d830ff46bfbb7c80bf4bc0e8bc31038cd
igor <igor.ramos.c@hotmail.com>
2022-08-16 08:42:17 -0500

    nuevos componentes y animaciones agregadas

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
+import "../style-page/Login.css";
+import ContentImageLogin from "../components/ContentImageLogin";
 
 function Login() {
+  const [estateContet, setEstateContet] = useState(true);
   return (
     <div className="container-login">
-      <Header />
+      <Header setEstateContet={setEstateContet} />
+      <ContentImageLogin estateContet={estateContet} />
     </div>
   );
 }
