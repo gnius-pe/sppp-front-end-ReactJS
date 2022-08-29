0b8367d0faef3a81865063407925ccad6beb136c
edilfonso2019 <benjaminedilfonso@gmail.com>
2022-08-29 12:15:51 -0500

    agregue portada

--- src/components/ContentImageLogin.jsx
+++ src/components/ContentImageLogin.jsx
@@ -1,10 +1,12 @@
+import iconlogin from "../assets/img/portada.jpg";
 import React from "react";
 import "../style-components/ContentImageLogin.css";
 
 function ContentImageLogin({ estateContet }) {
   return (
     <div className={`contenido ${estateContet ? "" : "traslation-element"}`}>
-      <h1>Aqui es para las imagenes</h1>
+
+      <img className="Portada" src={iconlogin} alt="Portada UNAS" />
     </div>
   );
 }
