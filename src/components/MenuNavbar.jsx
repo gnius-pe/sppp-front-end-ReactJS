8cd751238d21bb5b9befad2a92499d049b1eda4e
unknown <juan.omonte@unas.edu.pe>
2022-08-31 10:22:04 -0500

    agregar icons

--- src/components/MenuNavbar.jsx
+++ src/components/MenuNavbar.jsx
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 
 import hambuergerMenu from "../assets/icon-button/hamburger-menu.svg";
 import "../style-components/MenuNavbar.css";
+import reglamentoIcon from "../assets/icons/icons-menu/icon-reglamento.svg";
+import organigramaIcon from "../assets/icons/icons-menu/icon-organigrama.svg";
+import descargaIcon from "../assets/icons/icons-menu/icon-descarga.svg";
+import investigacionIcon from "../assets/icons/icons-menu/icon-investigacion.svg";
+import estructuraIcon from "../assets/icons/icons-menu/icon-estructura.svg";
+import buttonDespliegue from "../assets/icon-button/icons-menu/flecha-despliegue.svg";
 
 function MenuNavbar({ setEstateContet }) {
   const [stateMenu, setStateMenu] = useState(true);
@@ -22,23 +28,30 @@ function MenuNavbar({ setEstateContet }) {
       </button>
       <div class={`content-menu ${stateMenu ? "active" : ""}`}>
         <ul class="list-menu">
-          <li><a href="#" class="item-menu">Reglamento de PPP</a></li><br /><br /><br />
-          <li><a href="#" class="item-menu">Organigrama Comision PPP</a></li><br /><br /><br />
-          <li><a href="#" class="item-menu">Descarga de Formatos</a>
+          <li><img src={reglamentoIcon} alt="" /><a href="#" class="item-menu">Reglamento de PPP</a></li><br /><br /><br />
+          <li><img src={organigramaIcon} alt="" /><a href="#" class="item-menu">Organigrama Comision PPP</a></li><br /><br /><br />
+          <li><img src={descargaIcon} alt="" /><a href="#" class="item-menu">Descarga de Formatos  </a><img src={buttonDespliegue} alt="" />
             <ul class="submenu">
                 <li><a href="#">Formato solicitud</a></li>
                 <li><a href="#">Formato aceptacion</a></li>
                 <li><a href="#">Formato F1</a></li>
             </ul>
           </li>
-          <li><a href="#" class="item-menu">Programa de Investigacion</a>
+          <li><img src={investigacionIcon} alt="" /><a href="#" class="item-menu">Programa de Investigacion  </a><img src={buttonDespliegue} alt="" />
             <ul class="submenu">
               <li><a href="#">Tecnologias de la informacion</a></li>
               <li><a href="#">Sistemas de informacion</a></li>
               <li><a href="#">Ingienieria de software</a></li>
             </ul>
           </li>
-          <li><a href="#" class="item-menu">Estructura de Informe final</a></li>
+          <li><a href="#" class="item-menu">Estados de Practica  </a><img src={buttonDespliegue} alt="" /><br />
+            <ul class="submenu">
+                <li><a href="#">Practicas Presentadas</a></li>
+                <li><a href="#">Practica iniciada</a></li>
+            </ul>
+          </li>
+          <br />
+          <li><img src={estructuraIcon} alt="" /><a href="#" class="item-menu">Estructura de Informe final</a></li>
         </ul>
       </div>
     </div>
