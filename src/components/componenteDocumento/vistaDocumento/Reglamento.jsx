4ee9e835ccfcae64ff3997ce329a8c0645cfccd0
edilfonso2019 <benjaminedilfonso@gmail.com>
2022-10-26 17:17:57 -0500

    agregue el modal view de pdf del reglamento pdf

--- src/components/componenteDocumento/vistaDocumento/Reglamento.jsx
+++ src/components/componenteDocumento/vistaDocumento/Reglamento.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import "../styleVistaDocumento/Reglamento.css";
+import iconDownload from "../../../assets/icons/icon-download.svg";
 function Reglamento() {
   return (
     <div className="contenedor-reglamento">
-      <div>
-        <h1>Reg</h1>
-        <p>/public</p>
+      <div className="reglamento-cabezera-principal">
+        <div className="cabezera-secundaria">
+          <h1>REGLAMENTO DE PRÁCTICAS PRE PROFESIONALES</h1>
+          <p>/public</p>
+        </div>
+
+        <button className="button-download">
+          {" "}
+          <a
+            className="link-down"
+            href="https://drive.google.com/u/0/uc?id=1aGhbgNf6wT3OAUZKyx-gVVxQxl_7Bv54&export=download"
+          >
+            Descargar
+            <img className="icon-down" src={iconDownload} alt="" />
+          </a>
+        </button>
       </div>
+
       <div className="contenedor-documento">
         <div className="contain-pdf">
           <iframe
